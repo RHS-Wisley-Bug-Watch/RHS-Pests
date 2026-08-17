46c1b63d314897d17339dedba0a9463602be953b
Baron <156926246+Projavax@://github.com>
2026-08-17 11:34:26 +0100

    Retaining learning rate, increasing augmentations. Model 3.

--- insect_detection/main.py
+++ insect_detection/main.py
@@ -13,11 +13,11 @@ EXPERIMENT_CONFIG = {
     "learning_rate": 1e-3,   # 1e-3 (faster learning) or 1e-5 (slower/cautious)
     "epochs": 25,            # How many learning iterations
     "batch_size": 32,        # How many samples it looks at at once
-    "num_augmentations": 6,  # Augmenting only images with bugs in them for balance
+    "num_augmentations": 8,  # Augmenting only images with bugs in them for balance
     "threshold": 0.5,        # Model confidence for classification
     "frozen_layers": True,
     "architecture": "ResNet18",
-    "notes": "Changing learning rate. Model 2."
+    "notes": "Retaining learning rate, increasing augmentations. Model 3."
 }
 
 BASE_DIR = os.getcwd()
